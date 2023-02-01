34de8b82a8fc807bace08d705d1bbc4ea3544315
hwangblood <hwangblood@gmail.com>
2023-02-01 21:56:31 +0800

    Login UI

---
+++ lib/constants/widget_constant.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/theme/pallete.dart';
+
+class WidgetConstants {
+  static AppBar appBar() {
+    return AppBar(
+      centerTitle: true,
+      title: SvgPicture.asset(
+        AssetsConstants.twitterLogo,
+        color: Pallete.blueColor,
+        height: 30,
+      ),
+    );
+  }
+}
--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -8,8 +13,70 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final appBar = WidgetConstants.appBar();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: appBar,
+      body: Center(
+        child: SingleChildScrollView(
+          // controller: controller,
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              // textfield 1
+              AuthField(
+                controller: emailController,
+                hintText: 'Email',
+              ),
+              const SizedBox(height: 25),
+              // textfield 2
+              AuthField(
+                controller: passwordController,
+                hintText: 'Password',
+              ),
+              const SizedBox(height: 40),
+              // text button
+              Align(
+                alignment: Alignment.topRight,
+                child: RoundedSmallButton(
+                  onTap: () {},
+                  label: 'Done',
+                ),
+              ),
+              const SizedBox(height: 40),
+              RichText(
+                text: TextSpan(
+                  text: 'Don\'t have an account?',
+                  children: [
+                    TextSpan(
+                      text: ' Sign up',
+                      style: const TextStyle(
+                        color: Pallete.blueColor,
+                        fontSize: 16,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          // navigate to sign up
+                        },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.clear();
   }
 }
