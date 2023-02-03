4e5cce3ff707b6da57961891c3dac7ad4b1fd866
hwangblood <hwangblood@gmail.com>
2023-02-03 23:59:50 +0800

    Home Screen UI

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,18 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
-class HomeView extends ConsumerWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
 
   static route() => MaterialPageRoute(
         builder: (context) => const HomeView(),
       );
+}
+
+class _HomeViewState extends State<HomeView> {
+  final appBar = WidgetConstants.appBar();
+
+  int _page = 0;
+
+  void _onPageChange(int value) {
+    setState(() {
+      _page = value;
+    });
+  }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      body: Center(
-        child: Text('home'),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      body: IndexedStack(
+        index: _page,
+        children: const [
+          Text('home'),
+          Text('search'),
+          Text('notification'),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: Pallete.whiteColor,
+          size: 28,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _page,
+        onTap: _onPageChange,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        backgroundColor: Pallete.backgroundColor,
+        items: [
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                _page == 0
+                    ? AssetsConstants.homeFilledIcon
+                    : AssetsConstants.homeOutlinedIcon,
+                color: Pallete.whiteColor,
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsConstants.searchIcon,
+              color: Pallete.whiteColor,
+            ),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              _page == 2
+                  ? AssetsConstants.notifFilledIcon
+                  : AssetsConstants.notifOutlinedIcon,
+              color: Pallete.whiteColor,
+            ),
+            label: 'Notification',
+          ),
+        ],
       ),
     );
   }
