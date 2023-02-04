aaadfc26df3118de6c83b96c8c0278d9c37d11df
hwangblood <hwangblood@gmail.com>
2023-02-04 23:33:02 +0800

    Picking Multiple Images & Displaying in Carousel

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showSnackBar(BuildContext context, String content) {
   ScaffoldMessenger.of(context).showSnackBar(
@@ -10,4 +13,17 @@ void showSnackBar(BuildContext context, String content) {
 
 String getNameFromEmail(String email) {
   return email.split('@').first;
+}
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  final ImagePicker picker = ImagePicker();
+  final pickedFiles = await picker.pickMultiImage();
+  if (pickedFiles.isNotEmpty) {
+    for (var file in pickedFiles) {
+      images.add(File(file.path));
+    }
+  }
+
+  return images;
 }
--- lib/features/tweet/view/create_tweet_view.dart
+++ lib/features/tweet/view/create_tweet_view.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/core/utils.dart';
 import 'package:twitter_clone/features/auth/auth.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
@@ -20,6 +24,12 @@ class CreateTweetView extends ConsumerStatefulWidget {
 
 class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
   final tweetTextController = TextEditingController();
+  List<File> images = [];
+
+  Future<void> onPickImages() async {
+    images = await pickImages();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,14 +54,13 @@ class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
           ? const Loader()
           : SafeArea(
               child: SingleChildScrollView(
-                // controller: controller,
                 child: Column(
                   children: [
                     Row(
                       children: [
                         CircleAvatar(
                           backgroundImage: NetworkImage(currentUser.profilePic),
-                          radius: 30,
+                          radius: 25,
                         ),
                         const SizedBox(width: 15),
                         Expanded(
@@ -72,6 +81,20 @@ class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
                         ),
                       ],
                     ),
+                    if (images.isNotEmpty)
+                      CarouselSlider(
+                        items: images.map((file) {
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: const EdgeInsets.symmetric(horizontal: 5),
+                            child: Image.file(file),
+                          );
+                        }).toList(),
+                        options: CarouselOptions(
+                          height: 400,
+                          enableInfiniteScroll: false,
+                        ),
+                      ),
                   ],
                 ),
               ),
@@ -91,7 +114,9 @@ class _CreateTweetViewState extends ConsumerState<CreateTweetView> {
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset(AssetsConstants.galleryIcon),
+                child: GestureDetector(
+                    onTap: onPickImages,
+                    child: SvgPicture.asset(AssetsConstants.galleryIcon)),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
