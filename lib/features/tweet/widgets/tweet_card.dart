cfb1fd32e0486a990e4dc77ef6b4860a83c16b73
hwangblood <hwangblood@gmail.com>
2023-06-17 15:54:04 +0800

    Like Button Animation

--- lib/features/tweet/widgets/tweet_card.dart
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:any_link_preview/any_link_preview.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:like_button/like_button.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
 import 'package:twitter_clone/common/common.dart';
@@ -103,10 +105,31 @@ class TweetCard extends ConsumerWidget {
                                   text: (tweet.reshareCount).toString(),
                                   onTap: () {},
                                 ),
-                                TweetIconButton(
-                                  pathName: AssetsConstants.likeOutlinedIcon,
-                                  text: (tweet.likes.length).toString(),
-                                  onTap: () {},
+                                LikeButton(
+                                  size: 25,
+                                  likeBuilder: (isLiked) {
+                                    return isLiked
+                                        ? SvgPicture.asset(
+                                            AssetsConstants.likeFilledIcon,
+                                            color: Pallete.redColor,
+                                          )
+                                        : SvgPicture.asset(
+                                            AssetsConstants.likeOutlinedIcon,
+                                            color: Pallete.greyColor,
+                                          );
+                                  },
+                                  likeCount: tweet.likes.length,
+                                  countBuilder: (likeCount, isLiked, text) {
+                                    return Text(
+                                      text,
+                                      style: TextStyle(
+                                        color: isLiked
+                                            ? Pallete.redColor
+                                            : Pallete.whiteColor,
+                                        fontSize: 16,
+                                      ),
+                                    );
+                                  },
                                 ),
                                 IconButton(
                                   onPressed: () {},
